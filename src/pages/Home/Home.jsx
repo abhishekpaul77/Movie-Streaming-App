0964c172984831fea9f75202965548b8295551bc
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2023-10-15 20:55:15 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,43 @@
-import NavBar from "../../components/Navbar/NavBar"
-import Featured from "../../components/featured/Featured"
-import List from "../../components/list/List"
-import "./home.scss"
+import { useEffect, useState } from "react";
+import NavBar from "../../components/Navbar/NavBar";
+import Featured from "../../components/featured/Featured";
+import List from "../../components/list/List";
+import "./home.scss";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({ type, genre }) => {
+  const [lists, setLists] = useState([]);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${
+            genre ? "&genre=" + genre : ""
+          }`,
+          {
+            headers: {
+              token: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MTU0N2MwMWFkM2M0NzYyNWNkMDY4MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5NjU3OTExOCwiZXhwIjoxNjk3MDExMTE4fQ.QB-1Hlj7UWariF7qUAPUSaWvSXhvbSfH3EzZXeBSwSY",
+            },
+          }
+        );
+        setLists(res.data)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists()
+  }, [type,genre]);
+
   return (
-    <div className='home'>
-     <NavBar/>
-     <Featured/>
-     <List/>
-     <List/>
-     <List/>
-     <List/>
+    <div className="home">
+      <NavBar />
+      <Featured type={type} />
+      {lists.map((list)=>(
+        <List list={list}/>
+      ))} 
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
