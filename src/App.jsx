4b6e0b3ba98a9a05ecac9bf184f2192f4f961518
Abhishek Paul <128720566+abhishekpaul77@users.noreply.github.com>
2023-10-15 20:54:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import './app.scss';
-import Login from './pages/login/Login';
-// import Register from './pages/register/Register';
-// import Home from "./pages/Home/Home";
-// import Watch from './pages/watch/Watch';
+import "./app.scss";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/Home/Home";
+import Watch from "./pages/watch/Watch";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div>
-      {/* <Home/> */}
-      {/* <Watch/> */}
-      {/* <Register/> */}
-      <Login/>
-    </div>
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={user ? <Home /> : <Navigate to="/register" />}
+      />
+      <Route
+        exact
+        path="/register"
+        element={!user ? <Register /> : <Navigate to="/" />}
+      />
+      <Route
+        exact
+        path="/login"
+        element={!user ? <Login /> : <Navigate to="/" />}
+      />
+      {user && (
+        <>
+          <Route path="/movies" element={<Home type="movies" />} />
+          <Route path="/series" element={<Home type="series" />} />
+          <Route path="/watch" element={<Watch/>} />
+        </>
+      )}
+    </Routes>
   );
 }
 
